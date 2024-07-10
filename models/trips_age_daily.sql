72537bc3bdc12128fc759abdb3fa9bb3baeec23c
Oksana Laputskaia <rariantia@gmail.com>
2024-07-10 12:39:56 +0000

    add transformations

--- models/trips_age_daily.sql
+++ models/trips_age_daily.sql
@@ -2,9 +2,10 @@ with
     cte as (
         select
            date,
-            extract(year from t.started_at) - extract(year from u.birth_date) as age
-        from {{ref("trips_prep")}}
-        inner join  {{ source("scooters_raw", "users") }} as u on t.user_id = u.id
-    ),
-    select date, age, count(*) as trips from cte 
-    group by 1, 2
+            extract(year from t.started_at) - extract(year from u.birth_date) as age,price_rub revenue_rub
+        from {{ref("trips_prep")}} t
+        join  {{ source("scooters_raw", "users") }} as u on t.user_id = u.id
+    )
+
+select date, age, count(*) as trips,sum(revenue_rub) revenue_rub from cte 
+group by 1, 2
