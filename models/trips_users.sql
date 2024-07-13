9a5cfc1de558676194ecac0c3ed550b6301349a7
Oksana Laputskaia <rariantia@gmail.com>
2024-07-13 07:43:38 +0000

    add increment

--- models/trips_users.sql
+++ models/trips_users.sql
@@ -1,19 +1,20 @@
 select
-    t.id,
-    t.user_id,
-    scooter_hw_id,
-    started_at,
-    finished_at,
-    start_lat,
-    start_lon,
-    finish_lat,
-    finish_lon,
-    distance_m,
-    price_rub,
-    duration_s,
-    is_free,
-    date,
-    sex,
+    t.*,
+    u.sex,
     extract(year from t.started_at) - extract(year from u.birth_date) as age
-from {{ ref("trips_prep") }} t
-left join {{ source("scooters_raw", "users") }} u on t.user_id = u.id
+from
+    {{ ref("trips_prep") }} as t
+left join
+    {{ source("scooters_raw", "users") }} as u
+    on t.user_id = u.id
+ {% if is_incremental() %}
+where
+    t.id > (select max(id) from {{ this }})
+order by
+    t.id
+limit
+    75000
+{% else %}
+where
+    t.id <= 75000
+{% endif %}
