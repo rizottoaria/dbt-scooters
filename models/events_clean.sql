581c89e22f1e3f5a021790e52c44e9698044d4e6
Oksana Laputskaia <rariantia@gmail.com>
2024-07-15 14:11:53 +0000

    minor changes

--- models/events_clean.sql
+++ models/events_clean.sql
@@ -1,7 +1,7 @@
 select distinct user_id, "timestamp", type_id
 from {{ source("scooters_raw", "events") }}
 {% if is_incremental() %}
-    "timestamp" > (select max("timestamp") from {{ this }})
+   where "timestamp" > (select max("timestamp") from {{ this }})
 {% else %}
-    "timestamp" < timestamp '2023-08-01'
+   where "timestamp" < '2023-08-01'::date
 {% endif %}
