ab6ca7d7667f311af58b01467e278b6a4b90e5fe
Oksana Laputskaia <rariantia@gmail.com>
2024-07-15 14:00:51 +0000

    add minor changes

--- models/events_clean.sql
+++ models/events_clean.sql
@@ -1,7 +1,7 @@
 select distinct user_id, "timestamp", type_id
 from {{ source("scooters_raw", "events") }}
-{% if is_incremental() %} 
-where timestamp < date '2023-08-01'
-{% else %} 
-where timestamp > (select max(timestamp) from {{ this }})
+{% if is_incremental() %}
+    "timestamp" > (select max("timestamp") from {{ this }})
+{% else %}
+    "timestamp" < timestamp '2023-08-01'
 {% endif %}
