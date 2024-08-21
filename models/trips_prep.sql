62e885775cbb0209dbd4c547dd5dc83e438917d9
Oksana Laputskaia <rariantia@gmail.com>
2024-08-21 08:22:43 +0000

    added macros use

--- models/trips_prep.sql
+++ models/trips_prep.sql
@@ -1,16 +1,18 @@
-select id, 
-user_id,
-	scooter_hw_id ,
-	started_at ,
-	finished_at ,
-	start_lat ,
-	start_lon ,
-	finish_lat,
-	finish_lon,
-	distance distance_m,
-	round(price/100,2) price_rub,
-    extract(epoch from (finished_at -started_at)) duration_s,
-    case when price=0 and extract(epoch from (finished_at -started_at))>0 then 1 else 0 end  is_free, 
- date(started_at) as date
-     from {{ source("scooters_raw", "trips") }}
 
+select
+    id,
+    user_id,
+    scooter_hw_id,
+    started_at,
+    finished_at,
+    start_lat,
+    start_lon,
+    finish_lat,
+    finish_lon,
+    distance as distance_m,
+    cast(price as decimal(20, 2)) / 100 as price_rub,
+    extract(epoch from (finished_at - started_at)) as duration_s,
+    finished_at <> started_at and price = 0 as is_free,
+    {{ date_in_moscow('started_at') }} as "date"
+from
+    {{ source("scooters_raw", "trips") }}
